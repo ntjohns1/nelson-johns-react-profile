4251fc9ca4c7a4ca4948f18608b19295719b6ceb
ntjohns1 <nelsontjohns@gmail.com>
2021-07-05 01:06:48 -0400

    successfully rendered resume with react-pdf

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+
 import './App.css';
 
 library.add(fab);
--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import samplePDF from './portfolioResume.pdf';
 // import {Navbar, Nav} from 'react-bootstrap';
 
-export default function Resume () {
-    return (
-        <div>
-            <p>
-                Resume Goes in Here
-            </p>
-        </div>
-    );
-};
+export default function Resume() {
+    const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <Document
+      file={samplePDF}
+      onLoadSuccess={onDocumentLoadSuccess}
+    >
+      {Array.from(
+        new Array(numPages),
+        (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+          />
+        ),
+      )}
+    </Document>
+  );
+}
