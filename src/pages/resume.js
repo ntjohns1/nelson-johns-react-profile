77dba324b316f6e96af984198486715eb0343acc
ntjohns1 <nelsontjohns@gmail.com>
2021-07-24 03:49:18 -0400

    fixing resume deployment

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page, pdfjs } from 'react-pdf';
+
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 import pdf from '../images/NelsonJohnsResume.pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default function Test() {
+export default function Resume() {
   return (
     <Container className='d-flex justify-content-center'>
-          <Document file={pdf}>
+          <Document file={pdf}  onLoadError={console.error}>
             <Page pageNumber={1} />
           </Document>
     </Container>
