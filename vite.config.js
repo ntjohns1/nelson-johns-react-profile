17f5f531610084165c9f0d986458fa6fc9c5dee7
ntjohns1 <nelsontjohns@gmail.com>
2025-03-29 15:28:36 -0400

    updates to package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/nelson-johns-react-profile/',
   build: {
     outDir: 'dist',
   },
